60af46cd0b5915bd4ec97519adda4dd6f87b6ea5
DevOps Faculty <dev@devops-school.local>
2026-04-06 07:18:47 -0400

    Add ESLint configuration for CI/CD pipeline code quality checks
    - Add eslint package to devDependencies
    - Add 'npm run lint' script for code linting
    - Create .eslintrc.js with Node.js/Express configuration
    - Enables linting checks in GitHub Actions workflows

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'coverage/', '.venv/'],
+};
